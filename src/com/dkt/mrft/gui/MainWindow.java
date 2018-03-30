bbfc7067ae6a1a18b5b6de555a3a624a691aeb20
Federico Vera <fede@riddler.com.ar>
2018-03-29 22:54:13 -0300

    Remove unused import

--- src/com/dkt/mrft/gui/MainWindow.java
+++ src/com/dkt/mrft/gui/MainWindow.java
@@ -54,7 +54,6 @@ import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.WindowAdapter;
